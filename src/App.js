5afd33c323064da4994e401461009c07116657ce
Amubasshir <themubasshir07@gmail.com>
2021-10-06 13:07:52 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
+import { getAuth, GithubAuthProvider, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import { useState } from "react";
 import './App.css';
+import initializeAuthentication from './Firebase/firebase.initialize';
 
-function App() {
+
+
+
+initializeAuthentication();
+const googleProvider = new GoogleAuthProvider();
+const gitHubProvider = new GithubAuthProvider();
+
+
+function App()
+{
+  const [user, setUser] = useState({});
+  const auth = getAuth();
+  const googleHandleSignIn = () =>
+  {
+   
+    signInWithPopup(auth, googleProvider)
+      .then( result =>
+      {
+        const {displayName, email, photoURL} = result.user;
+        const loggedInUser ={
+          name: displayName,
+          email: email,
+          image: photoURL,
+        };
+        setUser(loggedInUser);
+      })
+      .catch(error =>
+      {
+        console.log(error.message);
+    })
+  }
+
+  const handleGitHubSignIn = () =>
+  {
+    signInWithPopup(auth, gitHubProvider)
+      .then((result) =>
+      {
+        const { displayName, email, } = result.user;
+        const loggedInUser ={
+          name: displayName,
+          email: email,
+        };
+        setUser(loggedInUser);
+    })
+  }
+
+  const handleSignOut = () =>
+  {
+  
+    signOut(auth)
+    .then(() =>
+    {
+      setUser({});
+    })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        !user.name?
+        <div>
+      <button onClick={googleHandleSignIn}>google sign in</button>
+      <button onClick={handleGitHubSignIn}>github sign in</button>
+ </div> :
+      <button onClick={handleSignOut}>Sign out</button>}
+      <br />
+      {
+              user.name && <div>
+          <h2>Welcome {user.name} </h2>
+          <h6>fix { user.email} as soon as possible</h6>
+            </div>
+}
     </div>
   );
 }
---
+++ src/Firebase/Firebase.confiq.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCceLD3GerNGVdejYVDWGnDnrhReqwzAMA",
+    authDomain: "simple-firebase-login-cc965.firebaseapp.com",
+    projectId: "simple-firebase-login-cc965",
+    storageBucket: "simple-firebase-login-cc965.appspot.com",
+    messagingSenderId: "908697644164",
+    appId: "1:908697644164:web:af3e8697c8759fb952a6d1",
+    measurementId: "G-YB8M9R827P"
+  };
+
+  
+export default firebaseConfig;
